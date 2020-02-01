4569cec87a04c5d0ad079cf8338bbac78a4fc1f1
Beth <bethhnielsen56@gmail.com>
2020-02-01 13:56:05 -0600

    syling improved still troubleshooting cards

--- main.js
+++ main.js
@@ -1,43 +1,50 @@
 const student = [
     {
-    id: 'student',
-    name: 'Beth',
-    house: 'Ravenclaw'
+        name: "Beth",
+        house: "Ravenclaw",
+    },
+    {
+        name: "Beth",
+        house: "Ravenclaw",
+    },
+    {
+        name: "Beth",
+        house: "Ravenclaw",
     }
 ]
 
+
 const printToDom = (divId, textToPrint) => {
-  const selectedDiv = document.getElementById(divId);
-  selectedDiv.innerHTML = textToPrint;
+   const selectedDiv = document.getElementById(divId);
+   selectedDiv.innerHTML = textToPrint;
 };
 
 const studentCards = () => {
-    let domString= "";
-    for (let i = 0; i < student.length; i++) {
-        domString += `<div class="card text-white bg-form mb-3" style="max-width: 18rem;">`;
-        domString +=   `<div class="card-body">`;
-        domString +=     `<h1 class="card-title">${student[i].name}</h1>`;
-        domString +=     `<h3 class="card-text">${student[i].house}</h3>`;
-        domString +=     `<button class="btn btn-danger">Expel<button>`;
-        domString +=   `</div>`;
-        domString += `</div>`;
-    }
-   printToDom("studentSort", domString); 
+     let domString= "";
+     for (let i = 0; i < student.length; i++) {
+         domString += '<div class="card text-white bg-form mb-3" style="max-width: 18rem;">';
+         domString += '<div class="card-body">';
+         domString += `<h1 class="card-title">${student[i].name}</h1>`;
+         domString += `<h3 class="card-text">${student[i].house}</h3>`;
+         domString += '</div>';
+         domString += '<button class="btn btn-danger">Expel<button>';
+         domString += '</div>';
+     }
+    console.log(printToDom('studentSort', domString)); 
 };
 
 
 const toggleFunction = () => {
     const toggle = document.getElementById("jumbotronBtnToggle");
-    if (toggle.style.display === "none") {
-        toggle.style = "block";
-    } else {
-        toggle.style.display = "none";
-    }
-}
+     if (toggle.style.display === "none") {
+         toggle.style = "block";
+     } else {
+         toggle.style.display = "none";
+     }
+ }
 
-
-const jumbotronEvents = () => {
-    document.getElementById("jumbotronBtnToggle").addEventListener('click', jumbotronEvents)
+ const jumbotronEvents = () => {
+     document.getElementById("jumbotronBtnToggle").addEventListener('click', jumbotronEvents)
 }
 
 const init = () => {
@@ -45,4 +52,4 @@ const init = () => {
     studentCards();
 }
 
-init();
+init()
