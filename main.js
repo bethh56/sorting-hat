b071933647d05bd31e6708d828fe5648d910e18e
Beth <bethhnielsen56@gmail.com>
2020-02-21 10:34:19 -0600

    wip

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-
+//PRINT TO DOM
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
  };
  
+ // STUDENT SORTING CARDS
  const studentCards = () => {
       let domString= "";
       for (let i = 0; i < student.length; i++) {
           domString += '<div class="col-md-6 col-lg-4">';
           domString +=   `<div id=${student[i].House} class="card text-white bg-form m-5" background-color: #eee;">`;
-          domString +=     `<div class="border border-white rounded">`
+          domString +=     `<div class="border border-white rounded">`;
           domString +=       `<div class="card-body">`;
           domString +=       `<h1 class="card-title">${student[i].Name}</h1>`;
           domString +=       `<h3 class="card-text">${student[i].House}</h3>`;
           domString +=     '</div>';
-          domString +=     '<div>'
-          domString +=      '<button id="expel" type="button" class="btn btn-outline-light">Expel<button>';
-          domString +=     '</div>'
+          domString +=     '<div>';
+          domString +=      '<button id="expel" type="button" class="expelBtn btn btn-dark btn-outline-light m-3 w-50 float-right">Expel</button>';
           domString +=     '</div>';
-          domString +=   `</div>`
+          domString +=     '</div>';
+          domString +=   `</div>`;
           domString += '</div>';
           
           
@@ -73,7 +74,6 @@ const submitted = (e) =>  {
 const submitName = () => {
     document.getElementById("name").addEventListener('input', getName);
     document.getElementById("submitBtn").addEventListener('click', submitted);
-    document.getElementById('formReset').value='';
 }
 
 let expel = () => {
@@ -82,9 +82,10 @@ let expel = () => {
 }
 
 const expelBtn = (e) => {
-    document.getElementById("expel").addEventListener('click', expel)
+    document.getElementsByClassName("expelBtn").addEventListener('click', expel)
 }
 
+// PAGE LOAD INIT
 const init = () => {
     submitName();
 }
